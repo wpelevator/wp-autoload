5dfe55b3e95b3886f72fd56f1ae588d2220467be
Kaspars Dambis <hi@kaspars.net>
2024-10-16 11:45:38 +0300

    Rename per wpcs

--- php/class-autoload.php
+++ php/class-autoload.php
@@ -14,38 +14,38 @@ class Autoload {
 
 	private $resolved = [];
 
-	public function add( $namespace, $directory_path ) {
-		$namespace_normalized = sprintf( '%s\\', rtrim( $namespace, '\\' ) ); // Ensure we never match partial namespaces.
+	public function add( $ns, $directory_path ) {
+		$ns_normalized = sprintf( '%s\\', rtrim( $ns, '\\' ) ); // Ensure we never match partial namespaces.
 
-		$this->paths[ $namespace_normalized ] = rtrim( $directory_path, '\\/' ); // Remove trailing slashes for consistent concat.
+		$this->paths[ $ns_normalized ] = rtrim( $directory_path, '\\/' ); // Remove trailing slashes for consistent concat.
 	}
 
 	public function init() {
 		spl_autoload_register( [ $this, 'load' ] );
 	}
 
-	public function load( $class ) {
-		if ( ! isset( $this->resolved[ $class ] ) ) {
-			foreach ( $this->paths as $namespace => $directory_path ) {
-				if ( 0 === strpos( $class, $namespace ) ) {
-					$files = $this->files_for_class( $directory_path, str_replace( $namespace, '', $class ) );
+	public function load( $classname ) {
+		if ( ! isset( $this->resolved[ $classname ] ) ) {
+			foreach ( $this->paths as $ns => $directory_path ) {
+				if ( 0 === strpos( $classname, $ns ) ) {
+					$files = $this->files_for_class( $directory_path, str_replace( $ns, '', $classname ) );
 
 					foreach ( $files as $file ) {
 						if ( is_readable( $file ) ) {
-							$this->resolved[ $class ] = $file; // Cache the resolved.
+							$this->resolved[ $classname ] = $file; // Cache the resolved.
 						}
 					}
 				}
 			}
 		}
 
-		if ( isset( $this->resolved[ $class ] ) ) {
-			require_once $this->resolved[ $class ];
+		if ( isset( $this->resolved[ $classname ] ) ) {
+			require_once $this->resolved[ $classname ];
 		}
 	}
 
-	protected function files_for_class( $directory, $class ) {
-		$parts = explode( '\\', strtolower( str_replace( '_', '-', $class ) ) ); // Map class name to file path segments.
+	protected function files_for_class( $directory, $classname ) {
+		$parts = explode( '\\', strtolower( str_replace( '_', '-', $classname ) ) ); // Map class name to file path segments.
 
 		array_unshift( $parts, $directory ); // Prepend the base directory.
 
